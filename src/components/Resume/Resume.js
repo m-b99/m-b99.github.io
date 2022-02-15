98d6815d475bbf6f22fe3c9ac7e9746976ecd67b
mohamed-baiba <mohamed_baiba@um5.ac.ma>
2022-02-15 11:08:07 +0100

    modified5

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
-import pdf from "../../Assets/Cv.pdf";
+import pdf from "../../Assets/cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 
 function Resume() {
